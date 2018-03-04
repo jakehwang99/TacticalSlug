195ee5af9e016224f6e24414c717d1fd158cf850
jakehwang99 <jakehwang99@gmail.com>
2018-03-03 17:58:03 -0800

    added some gui things
    give access to kenneth to  gui

--- src/GUIView.java
+++ src/GUIView.java
@@ -1,19 +1,48 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUIView {
-    
-    private JButton iAgree = new JButton("I Agree");
-    private JTextArea area = new JTextArea();
-    private JButton reset = new JButton("Reset");
-    private JRadioButton p1 = new JRadioButton("Single");
-    private JRadioButton p2 = new JRadioButton("Multiplayer");
-    private JTextField field = new JTextField("Single under construction");
 
     public GUIView(){
-        JFrame frame = new JFrame("TacticalSlug");
-        frame.setBounds(100, 100, 700, 500);
-        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setVisible(true);
+        //main frame of GUI
+        JFrame mainFrame = new JFrame("TacticalSlug");
+        mainFrame.setBounds(100, 100, 700, 500);
+        mainFrame.setDefaultCloseOperation(mainFrame.EXIT_ON_CLOSE);
+        mainFrame.setResizable(false);
+
+        //JPanel to for sign-in screen
+        JPanel signin = new JPanel();
+        signin.setLayout(null);
+
+        //textpane with the name of the program
+        JTextArea description = new JTextArea();
+        //description.set
+
+        //textarea with description of the program and the terms to using it
+
+
+        //email text box
+
+
+        //"i agree" button
+        JButton iAgree = new JButton("I Agree");
+        iAgree.setBounds((mainFrame.getWidth()/2)-40, 400, 80, 20 );
+
+        signin.add(iAgree);
+
+        //iAgree.setSize(new Dimension(30, 30));
+
+        iAgree.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("he agress :D");
+            }
+        });
+
+
+        mainFrame.getContentPane().add(signin);
+        //set it visible
+        mainFrame.setVisible(true);
     }
 }
